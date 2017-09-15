5ac9503fb84271a8a9b1dd45544811ac300ec64e
Jorge Proaño <jorge@hiddennodeproblem.com>
2017-09-15 14:16:51 -0400

    update

--- bin/bunyan-pretty.js
+++ bin/bunyan-pretty.js
@@ -4,3 +4,21 @@
 const bunyanPretty = require('../lib')
 
 process.stdin.pipe(bunyanPretty(process.stdout))
+
+process.on('SIGINT', function () { cleanupAndExit('SIGINT') })
+process.on('SIGQUIT', function () { cleanupAndExit('SIGQUIT') })
+process.on('SIGTERM', function () { cleanupAndExit('SIGTERM') })
+process.on('SIGHUP', function () { cleanupAndExit('SIGHUP') })
+process.on('SIGBREAK', function () { cleanupAndExit('SIGBREAK') })
+
+function cleanupAndExit (signal) {
+  // HARD EXIT
+  setTimeout(() => {
+    console.log(signal)
+    process.exit(0)
+  }, 500)
+}
+
+process.stdin.on('end', () => {
+  process.exit(0)
+})
